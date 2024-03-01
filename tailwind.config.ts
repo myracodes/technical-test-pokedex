b0479323132ba854d475d81b2acf033b6b88d061
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2024-03-01 18:53:21 +0100

    config: add colors to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,63 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        pokemon: ['"Pokemon Hollow"'],
+        roboto: ['Roboto'],
+      },
+      colors: {
+        grass: '#78C850',
+        fire: '#F08030',
+        water: '#6890F0',
+        bug: '#A8B820',
+        normal: '#A8A878',
+        flying: '#A890F0',
+        poison: '#A040A0',
+        electric: '#F8D030',
+        ground: '#E0C068',
+        fighting: '#C03028',
+        psychic: '#F85888',
+        rock: '#B8A038',
+        ghost: '#705898',
+        ice: '#98D8D8',
+        steel: '#B8B8D0',
+        dragon: '#7038F8',
+        dark: '#705848',
+        fairy: '#EE99AC',
       },
     },
   },
   plugins: [],
+  safelist: [
+    'bg-grass',
+    'bg-fire',
+    'bg-water',
+    'bg-bug',
+    'bg-normal',
+    'bg-flying',
+    'bg-poison',
+    'bg-electric',
+    'bg-ground',
+    'bg-fighting',
+    'bg-psychic',
+    'bg-rock',
+    'bg-ghost',
+    'bg-ice',
+    'bg-steel',
+    'bg-dragon',
+    'bg-dark',
+    'bg-fairy',
+  ],
 };
 export default config;
