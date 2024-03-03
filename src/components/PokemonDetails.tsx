0a7813b3fecf21705e8edbb117fad087d350b577
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2024-03-03 18:33:14 +0100

    feat: add Stats component

--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import PokemonType from './PokemonTypes';
 import EvolutionChain from './EvolutionChain';
 import Image from 'next/image';
+import Stats from './Stats';
 
 const GRAPHQL = graphql`
   query PokemonDetailsQuery($pokemonId: Int!) {
@@ -58,23 +59,30 @@ const GRAPHQL = graphql`
 export const PokemonDetails = ({ pokemonId }: { pokemonId: number }) => {
   const data = useLazyLoadQuery<PokemonDetailsQuery>(GRAPHQL, { pokemonId });
 
-  const pokemonGeneration = data.pokemon_v2_generation_by_pk;
   const info = data.pokemon_v2_pokemon_by_pk;
   const sprites = data.pokemon_v2_pokemonsprites_by_pk?.sprites;
   const evolutionChain = data.pokemon_v2_evolutionchain[0].pokemon_v2_pokemonspecies;
-
   const isLegendary = info?.pokemon_v2_pokemonspecy?.is_legendary;
 
   return (
     <div key={pokemonId} className="flex flex-col gap-8 items-start justify-center max-w-[600px] mx-auto my-8">
-      <span className="flex flex-row items-center gap-8">
-        <Link href={'/'} className="text-xl uppercase font-bold border-2 px-4 py-1 mt-3 rounded-full">
-          ← Back
-        </Link>
-        <h1 className="text-[3rem] capitalize">
-          {info?.name} #{pokemonId.toString().padStart(3, '0')}
-        </h1>
-      </span>
+      <div className="flex flex-col gap-2 w-full">
+        <div className="flex flex-row items-center justify-center gap-8">
+          <Link
+            href={'/'}
+            className="text-lg uppercase font-bold border-2 px-4 py-1 mt-3 rounded-full border-dark-500 dark:border-neutral-200 hover:bg-neutral-200 dark:hover:bg-dark-300"
+          >
+            ← Back
+          </Link>
+          <h1 className="text-[2rem] capitalize grow">
+            {info?.name} #{pokemonId.toString().padStart(3, '0')}
+          </h1>
+        </div>
+        <span className="flex flex-row justify-end gap-2 uppercase font-bold text-xl text-end">
+          Gen. <p className="font-pokemon"># {info?.pokemon_v2_pokemonspecy?.generation_id}</p>
+        </span>
+      </div>
+
       <span className="flex flex-row justify-between gap-4 w-full">
         <span className="flex flex-row items-center gap-4">
           {info?.type.map((type: any) => (
@@ -89,17 +97,19 @@ export const PokemonDetails = ({ pokemonId }: { pokemonId: number }) => {
         </span>
 
         {isLegendary && (
-          <span className="flex flex-row items-center gap-2 border-2 px-4 py-1 rounded-full justify-self-end ">
+          <span className="flex flex-row items-center gap-2 border-2 px-4 py-1 rounded-full justify-self-end border-neutral-800">
             <p className="uppercase font-bold">Legendary</p>
             <Image src="/img/golden-pokeball.png" alt="legendary pokemon" width={22} height={22}></Image>
           </span>
         )}
       </span>
 
-      <p>Weight: {Number(info?.weight) / 10}kg</p>
-      <p>Height: {Number(info?.height) / 10}m</p>
-      <p>Capture rate: {info?.pokemon_v2_pokemonspecy?.capture_rate}</p>
-      <p className="capitalize">Generation {info?.pokemon_v2_pokemonspecy?.generation_id}</p>
+      <Stats
+        weight={Number(info?.weight) / 10}
+        height={Number(info?.height) / 10}
+        captureRate={info?.pokemon_v2_pokemonspecy?.capture_rate}
+      />
+
       <span className="flex flex-row mx-auto">
         <img src={sprites?.front_default} alt={info?.name} className="mx-auto" />
         {sprites?.front_shiny && <img src={sprites?.front_shiny} alt={info?.name} className="mx-auto" />}
@@ -108,7 +118,7 @@ export const PokemonDetails = ({ pokemonId }: { pokemonId: number }) => {
       </span>
       {evolutionChain && <EvolutionChain evolutionChain={evolutionChain}></EvolutionChain>}
 
-      {/* @TODO: ajouter différents tabs avec les stats, les évolutions, d'autres informations... */}
+      {/* @TODO: ajouter différents onglets avec les stats, les évolutions, d'autres informations... */}
     </div>
   );
 };
