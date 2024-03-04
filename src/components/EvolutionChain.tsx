f5ab412f5c0b1c7db389c68396528a8c970b0ee4
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2024-03-04 10:56:33 +0100

    chore: use a variable for clarity

--- src/components/EvolutionChain.tsx
+++ src/components/EvolutionChain.tsx
@@ -7,23 +7,26 @@ export const EvolutionChain = ({ evolutionChain }: { evolutionChain: any }): JSX
     <div className="flex flex-col gap-8 w-full">
       <h2 className="mx-auto">Evolution chain - First Gen:</h2>
       <span className="flex flex-row items-baseline gap-12 mx-auto">
-        {evolutionChain.map((evolution: any) => (
-          <Link
-            key={evolution.pokemon_v2_pokemons[0].evolution_id}
-            href={`/pokemon?id=${evolution.pokemon_v2_pokemons[0].evolution_id}`}
-            className="flex flex-col gap-2 w-fit"
-          >
-            <img
-              src={evolution.pokemon_v2_pokemons[0].pokemon_v2_pokemonsprites[0].sprites.front_default}
-              alt={evolution?.pokemon_v2_pokemons[0].name}
-              className="scale-125"
-            />
-            <span className="flex flex-row items-center justify-between tracking-wide capitalize ">
-              <p className="font-bold">{evolution.pokemon_v2_pokemons[0].name}</p>
-              <p className="font-pokemon">#{evolution.pokemon_v2_pokemons[0].evolution_id}</p>
-            </span>
-          </Link>
-        ))}
+        {evolutionChain.map((evolution: any) => {
+          const evol = evolution.pokemon_v2_pokemons[0];
+          return (
+            <Link
+              key={evol.evolution_id}
+              href={`/pokemon?id=${evol.evolution_id}`}
+              className="flex flex-col gap-2 w-fit"
+            >
+              <img
+                src={evol.pokemon_v2_pokemonsprites[0].sprites.front_default}
+                alt={evol.name}
+                className="scale-125"
+              />
+              <span className="flex flex-row items-center justify-between tracking-wide capitalize ">
+                <p className="font-bold">{evol.name}</p>
+                <p className="font-pokemon">#{evol.evolution_id}</p>
+              </span>
+            </Link>
+          );
+        })}
       </span>
     </div>
   );
